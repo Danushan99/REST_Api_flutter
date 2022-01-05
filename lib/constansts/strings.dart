ea1fd357827a2f68617065a85a05a623d9f967dd
Danushan <it20051648@my.sliit.lk>
2022-01-05 18:56:58 +0530

    new

--- lib/constansts/strings.dart
+++ lib/constansts/strings.dart
@@ -1,5 +1,7 @@
 
 
+// ignore_for_file: non_constant_identifier_names
+
 class Strings {
   static String news_url =
       'http://newsapi.org/v2/everything?domains=wsj.com&apiKey=850a779a62cc411f9c80e5ead20cef5b';
--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -7,5 +7,8 @@ class API_Manager {
   void getNews() async {
     var client = http.Client();
     var response = await client.get(Uri.parse(Strings.news_url));
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+    }
   }
 }
